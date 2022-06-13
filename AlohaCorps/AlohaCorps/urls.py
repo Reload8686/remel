895e49a2a9251659434dd8261cf196f6c5dd9db4
Reload8686 <reload8686@gmail.com>
2022-06-13 22:05:06 +0700

    initial commit

--- AlohaCorps/AlohaCorps/urls.py
+++ AlohaCorps/AlohaCorps/urls.py
@@ -1,8 +1,10 @@
 """AlohaCorps URL Configuration"""
+#AlohaCorps/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("aloha_app_pro/", include("aloha_app_pro.urls")),
+    path("blog/", include("blog.urls")),
 ]
